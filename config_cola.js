35fbad18b4d8f40b05f716f05cd224bf64c2405b
allanbaque1510 <allanbaque1510@gmail.com>
2023-12-30 22:51:06 -0500

    agreganndo cola

---
+++ config_cola.js
@@ -0,0 +1,54 @@
+import amqp from "amqplib";
+
+//Variables
+const RABBIT_HOST = "192.168.100.5";
+const RABBIT_PORT = "15672";
+const RABBIT_USERNAME = "allan";
+const RABBIT_PASSWORD = "allan";
+const RABBIT_VHOST = "cola";
+
+//Coneccion
+const RABBITMQ_URL = `amqp://${RABBIT_USERNAME}:${RABBIT_PASSWORD}@${RABBIT_HOST}:${RABBIT_PORT}/${RABBIT_VHOST}`;
+
+class RabbitMQConfig {
+  constructor() {
+    this.channel = null;
+  }
+
+  async connect() {
+    try {
+      const conn = await amqp.connect(RABBITMQ_URL);
+      this.channel = await conn.createChannel();
+      console.log(`Conectado a Rabbit: ${RABBITMQ_URL}`);
+    } catch (error) {
+      console.log(error);
+      throw error;
+    }
+  }
+
+  async createQueue(queueName, options) {
+    await this.channel.assertQueue(queueName, options);
+  }
+
+  async publishToQueue(queueName, message) {
+    this.channel.sendToQueue(queueName, Buffer.from(message));
+    console.log(`Enviando mensaje a ${queueName} en ${RABBIT_VHOST}`);
+  }
+
+  async subToQueue(queueName, callback, options) {
+    await this.channel.consume(
+      queueName,
+      (msg) => {
+        const message = msg.content.toString();
+        callback(message);
+        this.channel.ack(msg);
+      },
+      options
+    );
+  }
+
+  async close() {
+    await this.channel.close();
+  }
+}
+export default RabbitMQConfig;
--- src/app.js
+++ src/app.js
@@ -1,20 +1,36 @@
 import express from "express";
 import morgan from "morgan";
-import authRoutes from './routes/auth.routes.js'
-import imageRoutes from './routes/image.routes.js'
-import cors from 'cors'
+import authRoutes from "./routes/auth.routes.js";
+import imageRoutes from "./routes/image.routes.js";
+import cors from "cors";
 import cookieParser from "cookie-parser";
 const app = express();
-app.use(cors({
-    origin:'http://localhost:5173',
-    credentials:true
-}));
-app.use(express.json({ limit: '50mb' }));
-app.use(express.urlencoded({ limit: '50mb', extended: true }));
-app.use(morgan('dev'));
+const allowedOrigins = [
+  "http://192.168.100.87",
+  "http://192.168.100.88",
+  "http://192.168.100.86",
+  "http://192.168.100.5:5173",
+];
+
+const corsOptions = {
+  origin: function (origin, callback) {
+    // Verifica si el origen está en la lista de permitidos
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  credentials: true,
+};
+
+app.use(cors(corsOptions));
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb", extended: true }));
+app.use(morgan("dev"));
 app.use(express.json());
 app.use(cookieParser());
 
-app.use('/api',authRoutes);
-app.use('/api',imageRoutes);
+app.use("/api", authRoutes);
+app.use("/api", imageRoutes);
 export default app;
